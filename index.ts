d6a977612de76a12c1e4644e2ecb30d0a8366dde
Joshua Miller <joshdan65@gmail.com>
2020-10-15 19:39:45 -0500

    Add initial better-ajv-errors usage
    * Just testing things out at this point

--- index.ts
+++ index.ts
@@ -1,14 +1,23 @@
 import AJV from "ajv"
 import YAML from "yaml"
 import * as fs from "fs"
+import betterAjvErrors from 'better-ajv-errors'
 
-const ajValidator = new AJV({ allErrors: true });
+const ajValidator = new AJV({ allErrors: true, jsonPointers: true  });
 const communitySchema = JSON.parse(readFile("./schemas/someschema.json"));
 const validate = ajValidator.compile(communitySchema);
 
 const toValidate = YAML.parse(readFile("./testdata/something.yml"));
 
 const result = validate(toValidate);
+
+// if (!result) {
+//     const output = betterAjvErrors(communitySchema, toValidate, validate.errors, {
+//         format: "cli"
+//     });
+//     console.log(output);
+// }
+
 if (!result) {
     for (let index = 0; index < validate!.errors!.length; index++) {
         const element = validate!.errors![index];
